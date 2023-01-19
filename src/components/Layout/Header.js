502c41098e148f408d248453e96b70bda5ef0494
Callum Donaldson <callumdonaldson1990@gmail.com>
2023-01-19 15:39:04 +0000

    changed the button in the header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import classes from './Header.module.css'
-import mealsImage from '../../assets/meals.jpg'
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header() {
   return (
     <React.Fragment>
-        <header className={classes.header}>
-            <h1>Callum's Meals</h1>
-            <button>Cart</button>
-        </header>
-        <div className={classes['main-image']}>
-        <img src={mealsImage} alt="A table full of delicious food!"/>
-
-        </div>
-
-
+      <header className={classes.header}>
+        <h1>Callum's Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
     </React.Fragment>
-  )
+  );
 }
 
-export default Header
+export default Header;
