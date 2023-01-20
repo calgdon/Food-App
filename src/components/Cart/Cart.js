3c4f458c8d73d76ee54d806079cc8d9bdf2067cc
Callum Donaldson <callumdonaldson1990@gmail.com>
2023-01-20 09:44:24 +0000

    added in open and closes functionality to the modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,14 +13,14 @@ function Cart(props) {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
     {cartItems}
     <div className={classes.total}>
       <span>Total Amount</span>
-      <span>747474</span>
+      <span>£74.34</span>
     </div>
     <div className={classes.actions}>
-    <button className={classes['button--alt']}>Close</button>
+    <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
     <button className={classes.button}>Order</button>
 
     </div>
